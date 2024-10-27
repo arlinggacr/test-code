f8f9902ca4be49ff7cb56df8250ea9694e2ce256
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-10-28 00:09:20 +0700

    fix: change schema to adding middlewares at routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,16 +3,23 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 
-router.post("/", userController.createUser);
-
+router.post("/", authMiddleware, userController.createUser);
 router.get("/", userController.getAllUser);
 
-router.get("/account/:id", userController.getUserByAccountNumber);
+router.get(
+  "/account/:id",
+  authMiddleware,
+  userController.getUserByAccountNumber
+);
 
-router.get("/identity/:id", userController.getUserByIdentityNumber);
+router.get(
+  "/identity/:id",
+  authMiddleware,
+  userController.getUserByIdentityNumber
+);
 
 // adding some cases
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.put("/:id", authMiddleware, userController.updateUser);
+router.delete("/:id", authMiddleware, userController.deleteUser);
 
 module.exports = router;

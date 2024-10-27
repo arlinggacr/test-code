607f086982a291622512321b4e63d75babba80fa
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-10-28 00:48:30 +0700

    feat: update model user, into adding password, and also can get pre save that pasword will be generated as encrypted, and adding method to check the password

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,13 +1,26 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   accountNumber: { type: String, required: true, unique: true },
   emailAddress: { type: String, required: true, unique: true },
   identityNumber: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 userSchema.index({ accountNumber: 1 });
 userSchema.index({ identityNumber: 1 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);

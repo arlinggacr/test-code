2f3eaaed1c67949f6bc84fcddf76a8e1c08a62f3
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-10-28 00:08:56 +0700

    feat: making middlewares for authorization

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: "Token is missing, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
+
+module.exports = authMiddleware;
